6aabf122b9e1a6624d154dc25af26d79807af748
Sarah <sasou.eladdouli@orange.fr>
2021-10-22 11:37:45 +0200

    TP1 : exercice 3 Je n'ai pas encore instaurer les modes de difficultés

--- TP1_guessmyNumber_2_DUPUY_ELADDOULI/src/tp1_guessmynumber_2_dupuy_eladdouli/TP1_guessmyNumber_2_DUPUY_ELADDOULI.java
+++ TP1_guessmyNumber_2_DUPUY_ELADDOULI/src/tp1_guessmynumber_2_dupuy_eladdouli/TP1_guessmyNumber_2_DUPUY_ELADDOULI.java
@@ -5,6 +5,7 @@
 package tp1_guessmynumber_2_dupuy_eladdouli;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -18,17 +19,151 @@ public class TP1_guessmyNumber_2_DUPUY_ELADDOULI {
     public static void main(String[] args) {
              
       Random generateurAleat = new Random();
+      Scanner sc = new Scanner(System.in);
       
      
 //générateur de nombres aléatoires nommé generateurAleat
       int n = generateurAleat.nextInt(100); 
       int s = 0; //valeur que rentre lutilisateur
       
-       //System.out.println (n);
+        System.out.println (n);
         System.out.println ("saisir un nombre entre 0 et 100 : ");
+        s = sc.nextInt();
+        System.out.println (s);
+        
+        if (s<n)
+        {
+        System.out.println ("Trop petit");
+        }
+        
+        if (s==n)
+        {
+        System.out.println ("Gagné !");
+        }
+    
+        if (s>n)
+        {
+        System.out.println ("Trop grand");
+        }
+        
+        
+        int D = 0;
+        System.out.println ("Niveau de difficulté, \n1 (niveau facile), \n2 (niveau normal), \n3 (niveau difficile) : ");
+        D = sc.nextInt();
+        
+        
+        if (D==1)
+        {
+        
+        int e = 0; // new valeur 
+        System.out.println ("Saisir un NOUVEAU nombre entre 0 et 100 : ");
+        e = sc.nextInt();
+        
+        int compteur = 0;
+        
+        while (e!= n)
+        {
+        if (e<n)
+        {
+        System.out.println ("Trop petit");
+        System.out.println ("Nouvel essai : ");
+        e = sc.nextInt();
+        compteur = compteur +1;
+        }
+    
+        if (e>n)
+        {
+        System.out.println ("Trop grand");
+        System.out.println ("Nouvel essai : ");
+        e = sc.nextInt();
+        }
+
+         if (e==n)
+        {
+        System.out.println ("Gagné !");
+        }
+        }
+        
+         System.out.println ("Nombre de tentatives exécutées : " + compteur);
+        
+        }
+        
+        if (D==2)
+        {
+        
+        int e = 0; // new valeur 
+        System.out.println ("Saisir un NOUVEAU nombre entre 0 et 100 : ");
+        e = sc.nextInt();
+        
+        int compteur = 0;
+        
+        while (e!= n)
+        {
+        if (e<n)
+        {
+        System.out.println ("Trop petit");
+        System.out.println ("Nouvel essai : ");
+        e = sc.nextInt();
+        compteur = compteur +1;
+        }
+    
+        if (e>n)
+        {
+        System.out.println ("Trop grand");
+        System.out.println ("Nouvel essai : ");
+        e = sc.nextInt();
+        }
+
+         if (e==n)
+        {
+        System.out.println ("Gagné !");
+        }
+        }
+        
+         System.out.println ("Nombre de tentatives exécutées : " + compteur);
+        
+        }
+        if (D==3)
+        {
+        
+        int e = 0; // new valeur 
+        System.out.println ("Saisir un NOUVEAU nombre entre 0 et 100 : ");
+        e = sc.nextInt();
+        
+        int compteur = 0;
+        
+        while (e!= n)
+        {
+        if (e<n)
+        {
+        System.out.println ("Trop petit");
+        System.out.println ("Nouvel essai : ");
+        e = sc.nextInt();
+        compteur = compteur +1;
+        }
+    
+        if (e>n)
+        {
+        System.out.println ("Trop grand");
+        System.out.println ("Nouvel essai : ");
+        e = sc.nextInt();
+        }
+
+         if (e==n)
+        {
+        System.out.println ("Gagné !");
+        }
+        }
+        
+         System.out.println ("Nombre de tentatives exécutées : " + compteur);
+        
+        }
+        
+        
+        
         
     }
     
-        // TODO code application logic here
+     
  
 }
