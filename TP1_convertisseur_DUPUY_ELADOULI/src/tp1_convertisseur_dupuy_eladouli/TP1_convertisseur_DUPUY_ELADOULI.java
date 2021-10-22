bd71477ffcafbd8227d32d3bb4710df354a004a0
ROXANA <ROXANA@192.168.114.197>
2021-10-22 10:15:28 +0200

    EXO 2 Convertisseur question 3

--- TP1_convertisseur_DUPUY_ELADOULI/src/tp1_convertisseur_dupuy_eladouli/TP1_convertisseur_DUPUY_ELADOULI.java
+++ TP1_convertisseur_DUPUY_ELADOULI/src/tp1_convertisseur_dupuy_eladouli/TP1_convertisseur_DUPUY_ELADOULI.java
@@ -4,6 +4,8 @@
  */
 package tp1_convertisseur_dupuy_eladouli;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ROXANA
@@ -13,8 +15,24 @@ public class TP1_convertisseur_DUPUY_ELADOULI {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
         // TODO code application logic here
+           double TC = 0 ; 
+       Scanner sc = new Scanner(System.in);
+       System.out.println ("Température en Celsius : " + TC);
+       
+       TC = sc.nextDouble();
+       System.out.println ("Température en Kelvin " +(TC+273.15) );
+              
     }
-    
+     
+    public static double CelciusVersKelvin (double tCelcius) {
+      double Kelvin = 0 ;
+      Kelvin = (tCelcius + 273.15) ;
+      return Kelvin ;
+ }
+   
 }
+
+
