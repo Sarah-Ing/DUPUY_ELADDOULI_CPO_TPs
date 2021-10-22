46075b5d116c6f004e017e1c5a6f37c11ce31c2c
Sarah <sasou.eladdouli@orange.fr>
2021-10-22 10:05:58 +0200

    TP1 : EXERCICE 3

--- TP1_guessMyNumber_DUPUY_ELADDOULI/src/tp1_guessmynumber_dupuy_eladdouli/TP1_guessMyNumber_DUPUY_ELADDOULI.java
+++ TP1_guessMyNumber_DUPUY_ELADDOULI/src/tp1_guessmynumber_dupuy_eladdouli/TP1_guessMyNumber_DUPUY_ELADDOULI.java
@@ -4,17 +4,26 @@
  */
 package tp1_guessmynumber_dupuy_eladdouli;
 
+import java.util.Random;
+
 /**
  *
  * @author sarah
  */
 public class TP1_guessMyNumber_DUPUY_ELADDOULI {
 
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+           
+      Random generateurAleat = new Random();  //générateur de nombres aléatoires nommé generateurAleat
+      int n = generateurAleat.nextInt(100); 
+      int s = 0; //valeur que rentre lutilisateur
+        System.out.println ("saisir un nombre entre 0 et 100 : ");
+        
     }
     
 }
