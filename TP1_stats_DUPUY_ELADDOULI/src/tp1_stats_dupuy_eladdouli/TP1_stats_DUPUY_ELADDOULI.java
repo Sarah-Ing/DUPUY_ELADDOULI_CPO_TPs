7c72c1a9794c411a432988a9e5c24372d5fdee90
Sarah <sasou.eladdouli@orange.fr>
2021-10-22 12:28:31 +0200

    TP1 : exercice 4 Lance de dé problème resultat, seulement un de a 5 face

--- TP1_stats_DUPUY_ELADDOULI/src/tp1_stats_dupuy_eladdouli/TP1_stats_DUPUY_ELADDOULI.java
+++ TP1_stats_DUPUY_ELADDOULI/src/tp1_stats_dupuy_eladdouli/TP1_stats_DUPUY_ELADDOULI.java
@@ -4,6 +4,9 @@
  */
 package tp1_stats_dupuy_eladdouli;
 
+import java.util.Random;
+import java.util.Scanner;
+
 
 
 /**
@@ -16,8 +19,32 @@ public class TP1_stats_DUPUY_ELADDOULI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     
+     // L’objectif de cet exercice est vérifier si la distribution de nombres du générateur de nombre aléatoire est bien aléatoire, en simulant un lancer de dés à répétition.
+        Random generateurAleat = new Random();
+        Scanner sc = new Scanner(System.in);
         
+        int m = 0;
+        int s =0;
+        
+        int [] Tab = new int[5]; 
+        System.out.println ("Saisir un nombre entier : ");
+        m = sc.nextInt();
+    
+        for (int i=1; i <= m; i++) 
+        {  
+            s = generateurAleat.nextInt(5); 
+            Tab[s]++;
+            
+           //System.out.println (Tab[s]);
+        }
+        double d = 0;
+        
+        for (int j = 0; j <= 5; j++) 
+        {
+        d = (Tab[j]*100)/m;
+        System.out.println (d);
+        }
+         
     }
     
 }
